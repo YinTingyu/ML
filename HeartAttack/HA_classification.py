06e719e65e2be6129901467304728a53acf3bfa8
YinTingyu <122711888+YinTingyu@users.noreply.github.com>
2024-01-16 13:10:42 -0500

    Update HA_classification.py

--- HeartAttack/HA_classification.py
+++ HeartAttack/HA_classification.py
@@ -1,1 +1,13 @@
 # A guide to classification methods in heart attack analysis
+import os
+import numpy as np
+import pandas as pd
+import warnings
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pyplot.express as px
+pd.set_option("display.max_rows", None)
+from sklearn import preprocessing
+import matplotlib
+matplotlib.style.use('ggplot')
+from sklearn.preprocessing import LabelEncoder
